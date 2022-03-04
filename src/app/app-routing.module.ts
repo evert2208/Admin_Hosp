885d9ef8b93dcef29384395025f05ee409fcab44
evert2208 <65311634+evert2208@users.noreply.github.com>
2022-03-04 13:17:34 -0500

    diseño

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistroComponent } from './auth/registro/registro.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',
+  component:PagesComponent,
+  children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'progress', component: ProgressComponent},
+    {path: 'grafica1', component: Grafica1Component},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  ]
+  },
+
+  {path: 'login', component: LoginComponent},
+  {path: 'registro', component: RegistroComponent},
+
+
+
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
